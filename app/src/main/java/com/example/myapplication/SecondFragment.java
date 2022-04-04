a51346739ab9c63fc8088f7575981bd14f533bbe
JTZ18 <jon.taylorlim99@gmail.com>
2022-04-05 01:04:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/SecondFragment.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SecondFragment extends Fragment {
+
+    // Add RecyclerView member
+    private RecyclerView recyclerView;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+
+        // Add the following lines to create RecyclerView
+        recyclerView = view.findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView.setAdapter(new RandomNumListAdapter(1234));
+
+        return view;
+    }
+}
